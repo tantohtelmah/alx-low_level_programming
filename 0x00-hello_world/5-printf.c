79b6bb21ce71604c9ccd067d3a6b6ab83d9965da
tantohtelmah <tantohtelmah@gmail.com>
2023-08-10 19:11:32 -0400

    Task5 - printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+* main - prints text to standard output using 'printf'
+*
+* Return: Always zero
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
