e6bb295cf4f4d0e0c75b5accc08732889be8ccc6
tantohtelmah <tantohtelmah@gmail.com>
2023-08-10 19:02:48 -0400

    task 4 - betty changes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-int main(){
-	puts("Programming is like building a multilingual puzzle");
-	return 0;
+/**
+* main - prints text to standard output using 'puts'
+*
+* Return: Always zero
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
