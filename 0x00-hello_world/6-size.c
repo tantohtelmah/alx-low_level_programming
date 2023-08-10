7ae4b44f591c80c0af0d289eb95f1b4ee73a762a
tantohtelmah <tantohtelmah@gmail.com>
2023-08-10 19:58:31 -0400

    Checking diff

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
 /**
-* main - prints text to standard output using 'printf'
+* main - prints sizes of types
 *
 * Return: Always zero
 */
 int main(void)
 {
-	printf("Size of a char: %ld byte(s)  \n", sizeof(char));
-	printf("Size of a int: %ld byte(s) \n", sizeof(int));
+	printf("Size of a char: %ld byte(s)\n", sizeof(char));
+	printf("Size of an int: %ld byte(s)\n", sizeof(int));
 	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %ld byte(s) \n", sizeof(float));
+	printf("Size of a float: %ld byte(s)\n", sizeof(float));
 	return (0);
 }
