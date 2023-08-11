092123a95b1c66e260a80a07364f01dad1a648c0
tantohtelmah <tantohtelmah@gmail.com>
2023-08-10 20:53:56 -0400

    task8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+* main - prints text to standard error using 'fprintf'
+*
+* Return: Always zero
+*/
+int main(void)
+{
+	fprintf(strderr,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (0);
+}
