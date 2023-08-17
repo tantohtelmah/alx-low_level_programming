025b3e443b9dca0b9fecf7d54bc6c9498c012082
tantohtelmah <tantohtelmah@gmail.com>
2023-08-17 15:16:19 -0400

    Task2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <ctype.h>
+#include "_putchar.h"
+
+/**
+* mul - Chesks for digits
+* @a: integer
+* @b: integer
+* Return: Integer.
+*/
+
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}
