0d37cf1a0d108fb8a07ae322e5bf672579780888
tantohtelmah <tantohtelmah@gmail.com>
2023-08-17 16:00:55 -0400

    Task4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <ctype.h>
+#include <unistd.h>
+#include "_putchar.h"
+
+/**
+* print_most_numbers - Chesks for digits
+* Return: none
+*/
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		if (i == 50)
+		{
+			continue;
+		}
+		if (i == 50)
+		{
+			continue;
+		}
+		_putchar(i);
+	}
+	_putchar('\n');
+}
