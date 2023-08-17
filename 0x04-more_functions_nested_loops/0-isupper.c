c458e54e6914085767a63377cb37c023938f15aa
tantohtelmah <tantohtelmah@gmail.com>
2023-08-17 14:38:50 -0400

    Task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <ctype.h>
+#include "_putchar.h"
+
+/**
+* _isupper - Chesks for uppercase letters
+* @c: integer
+* Return: Integer.
+*/
+
+int _isupper(int c)
+{
+	if (isupper(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
---
+++ 0x04-more_functions_nested_loops/_putchar.h
@@ -0,0 +1,6 @@
+#ifndef _PUTCHAR_H_INCLUDED
+#define _PUTCHAR_H_INCLUDED
+
+int _putchar(int c);
+
+#endif /* _PUTCHAR_H_INCLUDED */
