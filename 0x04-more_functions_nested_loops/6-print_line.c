97d0239ae8b0ec871906da97601314fed02964a3
tantohtelmah <tantohtelmah@gmail.com>
2023-08-17 21:51:34 -0400

    debugging

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -13,12 +13,13 @@ void print_line(int n)
 {
 	int i;
 
-	for (i = 0; i <= n; i++)
-	}
+	for (i = 0; i < n; i++)
+	{
 		_putchar('_');
-		if (n <= 0)
+		if (n < 0)
 		{
 			_putchar('\n');
+			continue;
 		}
 	}
 	_putchar('\n');
