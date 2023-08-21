67d6ea082ef23e7848ae92ead7fa294f754162f6
tantohtelmah <tantohtelmah@gmail.com>
2023-08-21 13:31:07 -0400

    ..

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "_putchar.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+* _puts - prints out the string
+* @str: characters
+* Return: None
+*/
+
+void _puts(char *str)
+{
+	int len;
+	char str1;
+	
+	len = strlen(str);
+	str1 = str + len - 1;
+	while (str1 >= st)
+	{
+		puts(str1);
+		str1--;r
+}
