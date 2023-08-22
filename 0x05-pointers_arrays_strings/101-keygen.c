42efb946002535d4ced7e43fad812c7b275142f8
tantohtelmah <tantohtelmah@gmail.com>
2023-08-21 22:05:02 -0400

    task101

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define PASSWORD_LENGTH 10
+
+/**
+* main - password generate
+* Return: always zero
+*/
+
+int main(void)
+{
+	char password[PASSWORD_LENGTH + 1];
+	int index;
+	const char charset[];
+	int i;
+
+	charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+	srand(time(NULL));
+	for (i = 0; i < PASSWORD_LENGTH; i++)
+	{
+		index = rand() % (sizeof(charset) - 1);
+		password[i] = charset[index];
+	}
+	password[PASSWORD_LENGTH] = '\0';
+	printf("Generated password: %s\n", password);
+	return (0);
+}
