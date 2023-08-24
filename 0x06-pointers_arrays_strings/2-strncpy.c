7a0f7f4ca9658903f2b0226a1a12aa13da4bf2f1
tantohtelmah <tantohtelmah@gmail.com>
2023-08-24 01:52:18 -0400

    ..

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -15,7 +15,10 @@ char *_strncpy(char *dest, char *src, int n)
 
 	for (j = 0; j < n; j++)
 	{
-		*(dest + j)  = *(src + j);
+		if (j < (int)strlen(src))
+			*(dest + j)  = *(src + j);
+		else
+			*(dest + j) = '\0';
 	}
 	return (dest);
 }
