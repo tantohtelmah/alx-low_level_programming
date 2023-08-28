92167418d070700e21ba61718b000b3b748dd81c
tantohtelmah <tantohtelmah@gmail.com>
2023-08-28 07:55:39 -0400

    task2

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -13,27 +13,18 @@ char *_strstr(char *haystack, char *needle)
 {
 	int i;
 	int j;
-	char *start;
 
 	while (*(haystack + i) != '\0')
 	{
+		j = 0;
 		while (*(needle + j) != '\0')
 		{
-			if (*(haystack + i) == *(needle + j))
-			{
-				start = haystack;
-				while ((*(haystack + i) == *(needle + j)) && *(needle + j) != '\0')
-				{
-					return ((haystack + i));
-				}
-				if (*(needle + j) == '\0')
-				{
-					return ((start + i));
-				}
-			}
+			if (*(haystack + i + j) != *(needle + j))
+				break;
 			j++;
-			
 		}
+		if (*(needle + j) == '\0')
+			return (haystack + i);
 		i++;
 	}
 	return (NULL);
