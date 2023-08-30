b855053ddfc8b05148ccff8e3d8917786614a8dc
tantohtelmah <tantohtelmah@gmail.com>
2023-08-30 11:35:41 -0400

    task5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,13 +9,25 @@
 
 int _sqrt_recursion(int n)
 {
+	int last = 0;
+	int result = 1;
+
 	if (n == 0)
 	{
 		return (0);
 	}
-	if (n % n != 0)
+	if (result != last)
 	{
-		return (-1);
+		last = result;
+		result = (result + (n / result)) / 2;
 	}
-	return (n + _sqrt_recursion(n - 1));
+	if (result != last)
+	{
+		_sqrt_recursion(n);
+	}
+	if (n % 2 != 0)
+	{
+		result = -1;
+	}
+	return (result);
 }
