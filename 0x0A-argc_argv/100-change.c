6b849a9aca1a0cc61e83e639327bc285374586e2
tantohtelmah <tantohtelmah@gmail.com>
2023-09-01 12:35:59 -0400

    task5

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -2,8 +2,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define UNUSED(x) (void)(x)
-
 /**
  * main - greedy algorithm
  * @argc: int
@@ -13,49 +11,24 @@
 
 int main(int argc, char *argv[])
 {
-	int a, b, c, d, e, f, g, h, i, j, sum = 0;
-    int amt = atoi(*argv);
+	int sum = 0;
+	int amt;
 
-	if (amt == 1)
-    {
+	if (argc != 2)
+	{
 		printf("Error\n");
-    }
-	if (amt < 25)
-    {
-        a = amt / 25;
-        b = amt % 25;
-        amt = b;
-        sum += a;
-    }
-    if (amt < 10)
-    {
-        c = amt / 25;
-        d = amt % 25;
-        amt = d;
-        sum += c;
-    }
-    if (amt < 5)
-    {
-        e = amt / 25;
-        f = amt % 25;
-        amt = f;
-        sum += e;
-    }
-    if (amt < 25)
-    {
-        g = amt / 25;
-        h = amt % 25;
-        amt = h;
-        sum += g;
-    }
-    if (amt < 25)
-    {
-        i = amt / 25;
-        j = amt % 25;
-        amt = j;
-        sum += i;
-    }
-    printf("%d\n", sum);
-    UNUSED(argc);
-    return (0);
+		return (1);
+	}
+	amt = atoi(argv[1]);
+	sum = amt / 25;
+	amt = amt % 25;
+	sum += amt / 10;
+	amt = amt % 10;
+	sum += amt / 5;
+	amt = amt % 5;
+	sum += amt / 2;
+	amt = amt % 2;
+	sum += amt;
+	printf("%d\n", sum);
+	return (0);
 }
