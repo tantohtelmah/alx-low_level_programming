a5abf7481c0dda4a0c8a32e8192a162f284ea441
tantohtelmah <tantohtelmah@gmail.com>
2023-09-04 18:59:16 -0400

    task1..

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,8 +13,10 @@ char *create_array(unsigned int size, char c)
 	char *t;
 	unsigned int i;
 
+	if (size == 0)
+		return (NULL);
 	t = malloc(size * sizeof(char));
-	if (t != NULL && size != 0)
+	if (t != NULL)
 		for (i = 0; i < size; i++)
 			t[i] = c;
 	return (t);
