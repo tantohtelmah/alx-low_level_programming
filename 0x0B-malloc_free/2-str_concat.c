3823c430672c7295575c95377365b687ea9c19e0
tantohtelmah <tantohtelmah@gmail.com>
2023-09-04 23:38:41 -0400

    task3

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,25 +14,22 @@ char *str_concat(char *s1, char *s2)
 	char *x1 = s1;
 	char *x2 = s1;
 	char *t;
-	unsigned int i;
 
-	if (s1 == NULL || s2 == NULL)
+	t = malloc(strlen(s1) + strlen(s2) + 1);
+	if (t != NULL)
 	{
-		return (NULL);
-	}
-	t = (char *)malloc(strlen(s1) + strlen(s2) + 1);
-	for (i = 0; i > strlen(s1); i++)
-	{
-		t[i] = *(x1 + i);
-	}
-	for (i = (strlen(s1) - 2); i < strlen(s2); i++)
-	{
-		t[i] = *(x2 + i);
-	}
-	if (t == NULL)
-	{
-		return (NULL);
+		while (*x1 != '\0')
+		{
+			*t = *x1;
+			x1++;
+			t++;
+		}
+		while (*x2 != '\0')
+		{
+			*t = *x2;
+			x2++;
+			t++;
+		}
 	}
 	return (t);
-	free(t);
 }
--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,23 +15,26 @@ int **alloc_grid(int width, int height)
 	int i;
 	int j;
 
-	if (width == 0 || height == 0)
+	if (width <= 0 || (height <= 0))
 	{
 		return (NULL);
 	}
-	t = malloc((width * height) * 4);
-	for (i = 0; i > width; i++)
-	{
-		for (j = 0; i < height; j++)
-		{
-			t[i] = 0;
-		}
-	}
+	t = malloc(height * sizeof(int *));
 	if (t == NULL)
-	{
 		return (NULL);
-	}
 
+	for (i = 0; i < height; i++)
+	{
+		t[i] = malloc(width * sizeof(int));
+		if (t[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+				free(t[j]);
+			free(t);
+			return (NULL);
+		}
+		for (j = 0; j < width; j++)
+			t[i][j] = 0;
+	}
 	return (t);
-	free(t);
 }
