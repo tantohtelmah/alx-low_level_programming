0226bc7d137ed8503e2c566021483c61c4a51278
tantohtelmah <tantohtelmah@gmail.com>
2023-09-05 07:56:10 -0400

    ...

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -21,7 +21,7 @@ char *argstostr(int ac, char **av)
 	for (i = 0; i < ac; i++)
 		len += strlen(av[i]) + 1;
 
-	str = malloc(len * sizeof(char));
+	str = malloc(len * sizeof(char) + 1);
 	if (str == NULL)
 		return (NULL);
 
