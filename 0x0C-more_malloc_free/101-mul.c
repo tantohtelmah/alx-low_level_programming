507e8fc8aa6622bfe19317a9f05d88e9e113fcf3
tantohtelmah <tantohtelmah@gmail.com>
2023-09-07 12:41:56 -0400

    task5

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,16 +1,36 @@
 #include "main.h"
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * multiply - multiply number
- * @num1: int
- * @num2: int
+ * main - multiply number
+ * @argc: int
+ * @*argv[]: char
  * Return: int
 */
 
-int multiply(int num1, int num2)
-{
-	int mul = 0;
-
-	mul = num1 * num2;
-	return (mul);
+int main(int argc, char *argv[]) {
+    int num1, num2, result, i;
+    if (argc != 3) {
+        printf("Error\n");
+        exit(98);
+    }
+    for (i = 0; argv[1][i] != '\0'; i++) {
+        if (argv[1][i] < '0' || argv[1][i] > '9') {
+            printf("Error\n");
+            exit(98);
+        }
+    }
+    for (i = 0; argv[2][i] != '\0'; i++) {
+        if (argv[2][i] < '0' || argv[2][i] > '9') {
+            printf("Error\n");
+            exit(98);
+        }
+    }
+    num1 = atoi(argv[1]);
+    num2 = atoi(argv[2]);
+    result = num1 * num2;
+    printf("%d\n", result);
+    return 0;
 }
