56db61ea47c535afd4635f993d74b2169ac5a83b
tantohtelmah <tantohtelmah@gmail.com>
2023-09-07 11:55:15 -0400

    task4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -12,25 +12,30 @@
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *t;
+	char *new_ptr;
+	unsigned int i;
 
-	t = malloc(old_size);
-	if (new_size != old_size)
+	if (new_size == old_size)
+		return (ptr);
+	if (ptr == NULL)
 	{
-		if (new_size == old_size)
-			return (ptr);
-		if (new_size == 0 && ptr != NULL)
-		{
-			free(ptr);
+		new_ptr = malloc(new_size);
+		if (new_ptr == NULL)
 			return (NULL);
-		}
-		if (ptr == NULL)
-			return (t);
-		ptr = t;
-		if (ptr == NULL)
-			return (NULL);
-		memcpy(ptr, t, new_size);
-		free(ptr);
+		return (new_ptr);
 	}
-	return (ptr);
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+    }
+	new_ptr = malloc(new_size);
+	if (new_ptr == NULL)
+		return (NULL);
+	if (old_size > new_size)
+		old_size = new_size;
+	for (i = 0; i < old_size; i++)
+		new_ptr[i] = ((char *)ptr)[i];
+	free(ptr);
+	return (new_ptr);
 }
