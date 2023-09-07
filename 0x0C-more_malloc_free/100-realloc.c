b5013f645dd5d6fa807bd665c747171665476319
tantohtelmah <tantohtelmah@gmail.com>
2023-09-07 11:57:09 -0400

    task4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -28,7 +28,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	{
 		free(ptr);
 		return (NULL);
-    }
+	}
 	new_ptr = malloc(new_size);
 	if (new_ptr == NULL)
 		return (NULL);
