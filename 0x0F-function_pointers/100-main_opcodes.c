6d679d3229195ab6343bd6933661ae76b006ec6f
tantohtelmah <tantohtelmah@gmail.com>
2023-09-14 16:48:49 -0400

    task4

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints opcodes
+ * @argc: int
+ * @argv: char
+ * Return: none
+*/
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	int num_bytes = atoi(argv[1]);
+
+	if (num_bytes < 0)
+	{
+		printf("Error\n");
+		return (2);
+	}
+	unsigned char *ptr = (unsigned char *)main;
+
+	for (int i = 0; i < num_bytes; i++)
+	{
+		printf("%02x ", *(ptr + i));
+	}
+	printf("\n");
+	return (0);
+}
