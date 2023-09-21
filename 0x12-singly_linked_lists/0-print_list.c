74ff98d04d97619e85411f2760f05f005c26f2d1
tantohtelmah <tantohtelmah@gmail.com>
2023-09-21 13:12:03 -0400

    task0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stddef.h>
+
+/**
+ * print_list - prints a lists
+ * @h: list_t
+ * Return: size_t
+*/
+
+size_t print_list(const list_t *h)
+{
+	const list_t *current = h;
+	int count = 0;
+
+	while (current != NULL)
+	{
+		if (current->str == NULL)
+		{
+			printf("[%d] ", 0);
+			printf("%s\n", "nil");
+		}
+		else
+		{
+			printf("[%d] ", current->len);
+			printf("%s\n", current->str);
+		}
+		current = current->next;
+	}
+	count += 2;
+	return (count);
+}
---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,20 @@
+#ifndef LIST_H
+#define LIST_H
+#include <stddef.h>
+
+/**
+ * struct list - struct
+ * @len: int
+ * @str: char
+ * @next: struct list*
+*/
+typedef struct list
+{
+	char *str;
+	int len;
+	struct list *next;
+} list_t;
+
+size_t print_list(const list_t *h);
+
+#endif /* LIST_H */
