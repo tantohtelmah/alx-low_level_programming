e778c89fa6b68ba7cd6cc79dd1c2f004866f983f
tantohtelmah <tantohtelmah@gmail.com>
2023-09-21 22:55:23 -0400

    101task

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,19 +1,13 @@
+global main
 extern printf
-section .data
-    hello db 'Hello, Holberton', 0
-    format db '%s\n', 0
-
-section .text
-    global main
 
 main:
     ; call printf with hello and format string
-    mov rdi, format
-    mov rsi, hello
-    xor rax, rax
+    mov edi, format
+    xor eax, eax
     call printf
 
     ; exit program with status code 0
-    xor rdi, rdi
-    mov rax, 60
-    syscall
+    mov eax, 0
+    ret
+format: db `Hello, Holberton\n`, 0
