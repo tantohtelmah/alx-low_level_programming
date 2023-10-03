e8c4af5dbe66de64d9780996fd7fa4061dfcd11e
tantohtelmah <tantohtelmah@gmail.com>
2023-10-02 22:19:50 -0400

    task0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * read_textfile - read text file
+ * @filename: char
+ * @letters: size_t
+ * Return: ssize_t
+*/
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int open_file;
+	int read_file;
+	int write_file;
+	char *buff;
+
+	open_file = open(filename, O_RDONLY);
+	buff = malloc(letters);
+	if (buff == NULL || filename == NULL)
+	{
+		close(open_file);
+		return (0);
+	}
+	read_file = read(open_file, buff, letters);
+	write_file = write(STDOUT_FILENO, buff, letters);
+	if (open_file == -1 && read_file == -1 && write_file == -1)
+	{
+		close(open_file);
+		return (0);
+	}
+	free(buff);
+	close(open_file);
+	return (write_file);
+}
