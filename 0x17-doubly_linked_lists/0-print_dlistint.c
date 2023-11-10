ad389849470cb3a0ac1dba84470febf31f006c2b
tantohtelmah <tantohtelmah@gmail.com>
2023-11-09 20:50:18 -0500

    task0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,5 +1,9 @@
 #include "lists.h"
-
+/**
+ * print_dlistint - prints all the elements of a linked list
+ * @h: dlistint_t
+ * Return: size_t
+*/
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t count = 0;
