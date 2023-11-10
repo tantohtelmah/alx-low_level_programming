f9575378269287f04d0e5df6193ddafc0e8dc052
tantohtelmah <tantohtelmah@gmail.com>
2023-11-09 21:12:29 -0500

    task3

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -9,6 +9,9 @@ size_t dlistint_len(const dlistint_t *h)
 	size_t count = 0;
 
 	while (h != NULL)
+	{
 		count++;
+		h = h->next;
+	}
 	return (count);
 }
---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+* add_dnodeint_end - adds a new node to the end of the linked list
+* @head: dlistint_t
+* @n: int
+* Return: dlistint_t
+*/
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node = malloc(sizeof(dlistint_t));
+	dlistint_t *last_node = *head;
+
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+	new_node->next = NULL;
+	if (*head == NULL)
+	{
+		new_node->prev = NULL;
+		*head = new_node;
+		return (new_node);
+	}
+	while (last_node->next != NULL)
+		last_node = last_node->next;
+	last_node->next = new_node;
+	new_node->prev = last_node;
+	return (new_node);
+}
